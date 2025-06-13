09b66968626d822d91d29709d966acb39c726633
Povilas Dailyda <povilas.dailyda@gmail.com>
2025-06-13 10:17:33 +0300

    up

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { PageError } from './PageError.js';
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => res.send(new PageTemplate().render()));
 app.get('/start', (req, res) => res.send(new PageStart().render()));
 app.get('/end', (req, res) => res.send(new PageEnd().render()));
